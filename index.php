18234aa72640ecb23f8f12227bbefc354a3da57f
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2018-03-03 19:57:07 +0200

    Updated

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 switch ($_REQUEST['q']) {
     case '/sleep':
-         file_put_contents('/tmp/out.log', time()." Called sleep \n");
+         file_put_contents('/tmp/out.log', time()." Called sleep \n", FILE_APPEND);
          $data = ['res'=>'sleep'];
          break;
     case '/formalno':
-        file_put_contents('/tmp/out.log', time()." Called formalno \n");
+        file_put_contents('/tmp/out.log', time()." Called formalno \n", FILE_APPEND);
         $data = ['res'=>'formalno'];
         break;
     default:
-        file_put_contents('/tmp/out.log', time()." Called [DEFAUALT] \n");
+        file_put_contents('/tmp/out.log', time()." Called [DEFAUALT] \n", FILE_APPEND);
         $data = ['res'=>'default'];
 }
 
