d0a192568f91ede9aafa38296a09dd23a5daed8d
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2018-03-03 19:24:55 +0200

    Updated initial

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-
-print_r("SUCCESS");
-exit(0);
+switch ($_SERVER['REQUEST_URI']) {
+    case '/sleep':
+         file_put_contents('/tmp/out.log', time()." Called sleep \n");
+         break;
+    case '/formalno':
+        file_put_contents('/tmp/out.log', time()." Called formalno \n");
+        break;
+    default:
+        file_put_contents('/tmp/out.log', time()." Called [DEFAUALT] \n");
+}
