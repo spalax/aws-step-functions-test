4b607e063d0d7389d66a0ea87aca7eca8feda31e
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2018-03-03 19:27:02 +0200

    Updated

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 switch ($_SERVER['REQUEST_URI']) {
     case '/sleep':
          file_put_contents('/tmp/out.log', time()." Called sleep \n");
+         $data = ['res'=>'sleep'];
          break;
     case '/formalno':
         file_put_contents('/tmp/out.log', time()." Called formalno \n");
+        $data = ['res'=>'formalno'];
         break;
     default:
         file_put_contents('/tmp/out.log', time()." Called [DEFAUALT] \n");
+        $data = ['res'=>'default'];
 }
+
+
+header('Content-Type: application/json');
+echo json_encode($data);
